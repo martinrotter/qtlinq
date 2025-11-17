5e909b329e8bddd72d53effc75c28155815a39d2
Martin Rotter <rotter.martinos@gmail.com>
2025-11-17 14:53:34 +0100

    build

--- tests/tst_qtlinq.cpp
+++ tests/tst_qtlinq.cpp
@@ -95,7 +95,7 @@ void TestQtLinq::selectMany_flatten() {
                 })
                 .toList();
 
-  QList<int> expected{1, 2, 3, 4, 5};
+  QList<int> expected{1, 2, 3, 4, 5, 6};
   QCOMPARE(flat, expected);
 }
 
